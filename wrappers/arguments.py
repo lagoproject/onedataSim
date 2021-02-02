0d9bf7e1287d71ac7b8c44b8c1fcbafd19933a97
rpaganmunoz <74665009+rpaganmunoz@users.noreply.github.com>
2021-02-02 15:20:28 +0100

    Parameters update

--- wrappers/arguments.py
+++ wrappers/arguments.py
@@ -93,6 +93,27 @@ def get_sys_args():
     #  echo -e "  -k <altitude, in cm>           : Fix altitude, even for predefined sites"
     parser.add_argument('-k', dest='k', required=True, type=float,
                        help='Fix altitude, even for predefined sites, in cm, float and scientific notation allowed')
+    #  echo -e "  -c <atm_model>           : Atmospheric Model even for predefined sites"
+    parser.add_argument('-c', dest='c', required=True,
+                       help='Atmospheric Model even for predefined sites. Note: Start number with E to use external atmospheres module')
+    #  echo -e "  -b <rigidity cutoff>           : Rigidity cutoff; 0 = disabled; value in GV = enabled"
+    parser.add_argument('-b', dest='b', required=True,
+                       help='Rigidity cutoff; 0 = disabled; value in GV = enabled')
+    #  echo -e "  -m <Low edge zenith angle>           : Low edge of zenith angle (THETAP) [deg]"
+    parser.add_argument('-m', dest='m', required=True,  type=float,
+                       help='Low edge of zenith angle (THETAP) [deg]')
+    #  echo -e "  -n <High edge zenith angle>           : High edge of zenith angle (THETAP) [deg]"
+    parser.add_argument('-n', dest='n', required=True,  type=float,
+                       help='High edge of zenith angle (THETAP) [deg]')
+    #  echo -e "  -r<Low primary particle energy>           : Lower limit of the primary particle energy (ERANGE) [GeV]"
+    parser.add_argument('-r', dest='r', required=True,  type=float,
+                       help='Lower limit of the primary particle energy (ERANGE) [GeV]')
+    #  echo -e "  -o<BX>           : Horizontal comp. of the Earth's mag. field"
+    parser.add_argument('-o', dest='o', required=True,  type=float,
+                       help='Horizontal comp. of the Earth mag. field')
+    #  echo -e "  -q<BZ>           : Vertical comp. of the Earth's mag. field"
+    parser.add_argument('-q', dest='q', required=True,  type=float,
+                       help='Vertical comp. of the Earth mag. field')
     #  echo -e "  -x                             : Enable other defaults (It doesn't prompt user for unset parameters)"
     parser.add_argument('-x', action='store_true', default=None,
                        help="Enable other defaults (It doesn\'t prompt user for unset parameters)")    
@@ -110,7 +131,7 @@ def get_sys_args():
     args_dict.update({'v':CORSIKA_VER})
     
     #project a.k.a codename
-    #it shoulds describe a simulation 
+    #it should describe a simulation 
     
     codename=args_dict['s'] +'_'+ str(args_dict['t']) +'_'+ str(args_dict['k']) +'_'+ args_dict['v'] +'_'+ args_dict['h']
     if args_dict['y'] is True:
