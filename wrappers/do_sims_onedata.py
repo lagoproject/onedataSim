10472363d6d4382426384f03a26f6da74c98f006
rpaganmunoz <74665009+rpaganmunoz@users.noreply.github.com>
2021-03-01 11:45:13 +0100

    SQaaS tests 4

--- wrappers/do_sims_onedata.py
+++ wrappers/do_sims_onedata.py
@@ -25,7 +25,7 @@ from threading import Thread
 from queue import Queue
 
 #
-from arguments import *
+from arguments import get_sys_args
 
 onedataSimPath = os.path.dirname(os.path.abspath(__file__))
 
@@ -91,19 +91,19 @@ def get_first_catalog_metadata_json(catcodename, orcid):
 
     with open(onedataSimPath+'/json_tpl/common_context.json', 'r') as file1:
         with open(onedataSimPath+'/json_tpl/catalog_corsika.json', 'r') \
-        as file2:
-                j = json.loads(file1.read())
-                j = _add_json(j, json.loads(file2.read()))
-                s = json.dumps(j)
-                s = s.replace('CATCODENAME', catcodename)
-                s = s.replace('ORCID', orcid)
-                return json.loads(s)
+            as file2:
+            j = json.loads(file1.read())
+            j = _add_json(j, json.loads(file2.read()))
+            s = json.dumps(j)
+            s = s.replace('CATCODENAME', catcodename)
+            s = s.replace('ORCID', orcid)
+            return json.loads(s)
 
 
 def get_catalog_metadata_activity(startdate, enddate):
 
     with open(onedataSimPath+'/json_tpl/catalog_corsika_activity.json', 'r') \
-    as file1:
+        as file1:
         j = json.loads(file1.read())
         s = json.dumps(j)
         s = s.replace('CATCODENAME', catcodename)
@@ -117,16 +117,16 @@ def _get_common_metadata_aux():
 
     with open(onedataSimPath+'/json_tpl/common_context.json', 'r') as file1:
         with open(onedataSimPath+'/json_tpl/common_dataset.json', 'r') \
-        as file2:
-                j = json.loads(file1.read())
-                j = _add_json(j, json.loads(file2.read()))
-                return j
+            as file2:
+            j = json.loads(file1.read())
+            j = _add_json(j, json.loads(file2.read()))
+            return j
 
 
 def _get_input_metadata(filecode):
 
     with open(onedataSimPath+'/json_tpl/dataset_corsika_input.json', 'r') \
-    as file1:
+        as file1:
         j = _get_common_metadata_aux()
         j = _add_json(j, json.loads(file1.read()))
         s = json.dumps(j)
