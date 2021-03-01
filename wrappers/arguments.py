dafd41f8de5219d3497490eaece6a410f70dee41
rpaganmunoz <74665009+rpaganmunoz@users.noreply.github.com>
2021-03-01 11:39:29 +0100

    SQaaS tests 4

--- wrappers/arguments.py
+++ wrappers/arguments.py
@@ -27,8 +27,8 @@ def _get_arti_params_json_md(arti_dict):
          "@type": "lago:ArtiParams",
          "lago:fluxTime": "P"+str(arti_dict['p'])+"S",
          "lago:highEnergyIntModel": arti_dict['h'],
-         "lago:detectorSite": \
-         "https://github.com/lagoproject/DMP/blob/0.0.1/defs/sitesLago.jsonld#"\
+         "lago:detectorSite":
+         "https://github.com/lagoproject/DMP/blob/0.0.1/defs/sitesLago.jsonld#"
          + arti_dict['s'],
          "lago:altitude": arti_dict['k'],
          "lago:modatm": arti_dict['c'],
@@ -44,7 +44,7 @@ def _get_arti_params_json_md(arti_dict):
          "lago:debug": arti_dict['d'],
          "lago:defaults": arti_dict['x'],
          "lago:highEnergyCutsSecondaries": arti_dict['a']
-        }
+         }
         ]}
     return j
 
@@ -78,95 +78,95 @@ def get_sys_args():
     #  echo -e "  -t <flux time> : \
     #    Flux time (in seconds) for simulations"
     parser.add_argument('-t', dest='t', required=True, type=int,
-                       help='Flux time (in seconds) for simulations')
+                        help='Flux time (in seconds) for simulations')
     #  echo -e "  -v <CORSIKA version> : \
     #    CORSIKA version"
     # fixed, every docker image has their own CORSIKA version
     #  echo -e "  -h <HE Int Model (EPOS|QGSII)> : \
     #    Define the high interaction model to be used"
-    parser.add_argument('-h', dest='h', required=True, 
-                       choices=['EPOS', 'QGSII'],
-                       help='Define the high interaction model to be used \
-                       <(EPOS|QGSII)>')
+    parser.add_argument('-h', dest='h', required=True,
+                        choices=['EPOS', 'QGSII'],
+                        help='Define the high interaction model to be used \
+                        <(EPOS|QGSII)>')
     #  echo -e "  -u <user name>                 : User Name."
     parser.add_argument('-u', dest='u', required=True,
-                       help='ORCID code describing user, plain usernames are \
-                       no allowed for publication')
+                        help='ORCID code describing user, plain usernames are \
+                        no allowed for publication')
     #  echo -e "  -s <site> : \
     #    Location (several options)"
-    parser.add_argument('-s', dest='s', required=True, 
-                       choices=["hess", "sac", "etn", "ber", "lim", "glr",
-                       "mch", "bga", "mge", "brc", "and", "mpc",
-                       "cha", "cid", "mor", "lsc", "mbo", "ccs"],
-                       help='Predefined LAGO site')
+    parser.add_argument('-s', dest='s', required=True,
+                        choices=["hess", "sac", "etn", "ber", "lim", "glr",
+                        "mch", "bga", "mge", "brc", "and", "mpc",
+                        "cha", "cid", "mor", "lsc", "mbo", "ccs"],
+                        help='Predefined LAGO site')
     #  echo -e "  -j <procs> : \
     #    Number of processors to use"
     parser.add_argument('-j', dest='j', default=1,
-                       help='Number of processors to use')
+                        help='Number of processors to use')
     #  echo -e "  -y : \
     #    Select volumetric detector mode (default=flat array)"
     parser.add_argument('-y', action='store_true', default=None,
-                       help='Select volumetric detector mode \
-                       (default=flat array)')
+                        help='Select volumetric detector mode \
+                        (default=flat array)')
     #  echo -e "  -e : \
     #    Enable CHERENKOV mode"
     parser.add_argument('-e', action='store_true', default=None,
-                       help='Enable CHERENKOV mode')
+                        help='Enable CHERENKOV mode')
     #  echo -e "  -d : \
     #    Enable DEBUG mode"
     parser.add_argument('-d', action='store_true', default=None,
-                       help='Enable DEBUG mode')
+                        help='Enable DEBUG mode')
     #  echo -e "  -a : \
     #    Enable high energy cuts for secondaries"
     parser.add_argument('-a', action='store_true', default=None,
-                       help='Enable high energy cuts for secondaries')
+                        help='Enable high energy cuts for secondaries')
     #  echo -e "  -k <altitude, in cm> : \
     #    Fix altitude, even for predefined sites"
     parser.add_argument('-k', dest='k', required=True, type=float,
-                       help='Fix altitude, even for predefined sites, in cm, \
-                       float and scientific notation allowed')
+                        help='Fix altitude, even for predefined sites, in cm, \
+                        float and scientific notation allowed')
     #  echo -e "  -c <modatm> : \
     #    Atmospheric Model even for predefined sites"
     parser.add_argument('-c', dest='c',
-                       help='Atmospheric Model even for predefined sites. \
-                       Note: Start number with E to use external atmospheres \
-                       module')
+                        help='Atmospheric Model even for predefined sites. \
+                        Note: Start number with E to use external atmospheres \
+                        module')
     #  echo -e "  -b <rigidity cutoff> : \
     #    Rigidity cutoff; 0 = disabled; value in GV = enabled"
     parser.add_argument('-b', dest='b',
-                       help='Rigidity cutoff; 0 = disabled; value in GV = \
-                       enabled')
+                        help='Rigidity cutoff; 0 = disabled; value in GV = \
+                        enabled')
     #  echo -e "  -m <Low edge zenith angle> : \
     #    Low edge of zenith angle (THETAP) [deg]"
     parser.add_argument('-m', dest='m', type=float,
-                       help='Low edge of zenith angle (THETAP) [deg]')
+                        help='Low edge of zenith angle (THETAP) [deg]')
     #  echo -e "  -n <High edge zenith angle> : \
     #    High edge of zenith angle (THETAP) [deg]"
     parser.add_argument('-n', dest='n', type=float,
-                       help='High edge of zenith angle (THETAP) [deg]')
+                        help='High edge of zenith angle (THETAP) [deg]')
     #  echo -e "  -r<Low primary particle energy> : \
     #    Lower limit of the primary particle energy (ERANGE) [GeV]"
     parser.add_argument('-r', dest='r', type=float,
-                       help='Lower limit of the primary particle \
+                        help='Lower limit of the primary particle \
                        energy (ERANGE) [GeV]')
     #  echo -e "  -i<Upper primary particle energy> : \
     #    Upper limit of the primary particle energy (ERANGE) [GeV]"
     parser.add_argument('-i', dest='i', type=float,
-                       help='Upper limit of the primary particle \
-                       energy(ERANGE) [GeV]')
+                        help='Upper limit of the primary particle \
+                        energy(ERANGE) [GeV]')
     #  echo -e "  -o<BX> : \
     #    Horizontal comp. of the Earth's mag. field"
     parser.add_argument('-o', dest='o', type=float,
-                       help='Horizontal comp. of the Earth mag. field')
+                        help='Horizontal comp. of the Earth mag. field')
     #  echo -e "  -q<BZ> : \
     #    Vertical comp. of the Earth's mag. field"
     parser.add_argument('-q', dest='q', type=float,
-                       help='Vertical comp. of the Earth mag. field')
+                        help='Vertical comp. of the Earth mag. field')
     #  echo -e "  -x : \
     #    Enable other defaults (It doesn't prompt user for unset parameters)"
     parser.add_argument('-x', action='store_true', default=None,
-                       help="Enable other defaults (It doesn\
-                       't prompt user for unset parameters)")
+                        help="Enable other defaults (It doesn\
+                        't prompt user for unset parameters)")
     #  echo -e "  -? : \
     #    Shows this help and exit."
     parser.add_argument('-?', action='help', help='Shows this help and exit.')
