aa52d0cf0d34a1c9c11159537b161356ba8ef5c9
AJRubio-Montero <antonio.rubio@ciemat.es>
2021-11-18 18:02:27 +0100

    Update args_sims.py

--- wrappers/args_sims.py
+++ wrappers/args_sims.py
@@ -134,8 +134,9 @@ def get_sys_args_S0():
     # added by HA - Now -a option expect the ecut value in GeV. 04/OCT/2021
     # parser.add_argument('-a', action='store_true', default=None,
     #                    help='Enable high energy cuts for secondaries')
-    parser.add_argument('-a', dest='a', type=int, default=0,
-                        help='Enable and set high energy cuts for secondaries')
+    parser.add_argument('-a', dest='a', type=float, default=0.0,
+                        help='Enable and set high energy cuts for secondaries, \
+                        0 = disabled; value in GeV = enabled')
     #  echo -e "  -k <altitude, in cm> : \
     #    Fix altitude, even for predefined sites"
     parser.add_argument('-k', dest='k', type=float,
@@ -149,7 +150,7 @@ def get_sys_args_S0():
                         module')
     #  echo -e "  -b <rigidity cutoff> : \
     #    Rigidity cutoff; 0 = disabled; value in GV = enabled"
-    parser.add_argument('-b', dest='b',
+    parser.add_argument('-b', dest='b', default=0.0,
                         help='Rigidity cutoff; 0 = disabled; value in GV = \
                         enabled')
     #  echo -e "  -m <Low edge zenith angle> : \
