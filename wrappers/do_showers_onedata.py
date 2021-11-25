df81ff38567c3ffc7fd74b0ba05d90e9ea0f45c7
AJRubio-Montero <antonio.rubio@ciemat.es>
2021-11-25 17:08:56 +0100

    Update do_showers_onedata.py

--- wrappers/do_showers_onedata.py
+++ wrappers/do_showers_onedata.py
@@ -76,7 +76,7 @@ def _get_prt_metadata(filecode):
 
 
 def get_dataset_metadata_S1_pri_sec(catcodename, filecode, startdate, end_date,
-                         arti_params_dict):
+                                    arti_params_dict):
 
     #TO BE DONE  (only illustrative)
 
@@ -95,7 +95,7 @@ def get_dataset_metadata_S1_pri_sec(catcodename, filecode, startdate, end_date,
 
 
 def get_dataset_metadata_S1_shw(catcodename, filecode, startdate, end_date,
-                         arti_params_dict):
+                                arti_params_dict):
 
     #TO BE DONE  (only illustrative)
 
@@ -114,7 +114,7 @@ def get_dataset_metadata_S1_shw(catcodename, filecode, startdate, end_date,
 
 
 def get_dataset_metadata_S1_prt(catcodename, filecode, startdate, end_date,
-                         arti_params_dict):
+                                arti_params_dict):
 
     #TO BE DONE  (only illustrative)
 
@@ -163,7 +163,7 @@ def producer_S1_pri_sec(catcodename, arti_params):
     osUtils.run_Popen_interactive(cmd)
 
 
-    with open('./' + catcodename + '/'+ catcodename + '.run', 'r') as file1:
+    with open('./' + catcodename + '/' + catcodename + '.run', 'r') as file1:
         print(file1)
         for z in file1.readlines():
             if z != "":
@@ -185,11 +185,11 @@ def producer_S1_shw(catcodename, arti_params):
     q = Queue()
 
     # clean a possible previous simulation
-    #if os.path.exists(catcodename):
+    # if os.path.exists(catcodename):
     #    shutil.rmtree(catcodename, ignore_errors=True)
 
-    #cmd = 'do_showers.sh ' + arti_params
-    #osUtils.run_Popen_interactive(cmd)
+    # cmd = 'do_showers.sh ' + arti_params
+    # osUtils.run_Popen_interactive(cmd)
 
 
     with open('./' + catcodename + '/'+ catcodename + '.shw.run', 'r') as file1:
@@ -212,14 +212,14 @@ def producer_S1_prt(catcodename, arti_params):
     q = Queue()
 
     # clean a possible previous simulation
-    #if os.path.exists(catcodename):
+    # if os.path.exists(catcodename):
     #    shutil.rmtree(catcodename, ignore_errors=True)
 
-    #cmd = 'do_showers.sh ' + arti_params
-    #osUtils.run_Popen_interactive(cmd)
+    # cmd = 'do_showers.sh ' + arti_params
+    # osUtils.run_Popen_interactive(cmd)
 
 
-    with open('./' + catcodename + '/'+ catcodename + '.pri.run', 'r') as file1:
+    with open('./' + catcodename + '/' + catcodename + '.pri.run', 'r') as file1:
         print(file1)
         for z in file1.readlines():
             if z != "":
@@ -227,9 +227,9 @@ def producer_S1_prt(catcodename, arti_params):
                 # "primaries.sh -w ${wdir} -r ${arti_path} -m ${prims}"
                 # else if pri.run is generated, per element ${i}:
                 # "cd $wdir; while ! cp -a ??$i ./; do sleep 5; done; \\
-                #  bzcat ??${i}.pri.bz2 | grep -v "#" | awk '{print log($2)/log(10.)}' | sort -g |	awk -v bins=${prims} -v id=${i} 'BEGIN{n=0; mine=100000; maxe=-100000; bins = bins * 1.}{t[int($1*bins)]++; n++; if ($1 < mine) mine=$1; if ($1 > maxe) maxe=$1;}END{printf("# # # prt\n");printf("# # Primary energy histogram for %06d using %d bins per decade\n", id, bins);printf("# # Three column format is:\n# # energy_bin total_per_bin fraction_per_bin\n"); for (i in t) {print 10**(i/bins), t[i], t[i]*1./n; frc+=t[i]*1./n;} printf("# # Total primaries: %ld (%.2f) Emin=%.2f GeV; Emax=%.2f GeV\n", n, frc, 10**mine, 10**maxe);}' > "00${i}.prt ; \\ 
+                #  bzcat ??${i}.pri.bz2 | grep -v "#" | awk '{print log($2)/log(10.)}' | sort -g |	awk -v bins=${prims} -v id=${i} 'BEGIN{n=0; mine=100000; maxe=-100000; bins = bins * 1.}{t[int($1*bins)]++; n++; if ($1 < mine) mine=$1; if ($1 > maxe) maxe=$1;}END{printf("# # # prt\n");printf("# # Primary energy histogram for %06d using %d bins per decade\n", id, bins);printf("# # Three column format is:\n# # energy_bin total_per_bin fraction_per_bin\n"); for (i in t) {print 10**(i/bins), t[i], t[i]*1./n; frc+=t[i]*1./n;} printf("# # Total primaries: %ld (%.2f) Emin=%.2f GeV; Emax=%.2f GeV\n", n, frc, 10**mine, 10**maxe);}' > "00${i}.prt ; \\
                 #  rm ??${i}.pri.bz2; cd .."
-              
+
                 print(z)
                 filecode = z.split("echo DAT")[1].split(" ")[0]
                 task = z
@@ -239,17 +239,17 @@ def producer_S1_prt(catcodename, arti_params):
 
 # ---- MAIN run ----
 
-simulation = ARTIwrapper(args_showers.get_sys_args_S1, get_dataset_metadata_S1_pri_sec,
+simulation = ARTIwrapper(args_showers.get_sys_args_S1,
+                         get_dataset_metadata_S1_pri_sec,
                          producer_S1_pri_sec)
 simulation.run()
 
-#simulation = ARTIwrapper(args_showers.get_sys_args_S1, get_dataset_metadata_S1_shw,
-#                         producer_S1_shw)
-#simulation.run()
-#
-#simulation = ARTIwrapper(args_showers.get_sys_args_S1, get_dataset_metadata_S1_prt,
-#                         producer_S1_prt)
-#simulation.run()
-
-
-
+# simulation = ARTIwrapper(args_showers.get_sys_args_S1,
+#                          get_dataset_metadata_S1_shw,
+#                          producer_S1_shw)
+# simulation.run()
+# 
+# simulation = ARTIwrapper(args_showers.get_sys_args_S1,
+#                          get_dataset_metadata_S1_prt,
+#                          producer_S1_prt)
+# simulation.run()
