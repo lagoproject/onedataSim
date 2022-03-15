4230d60a9a298f95fd6cfeff7ba80d5c4b95894b
AJRubio-Montero <antonio.rubio@ciemat.es>
2022-03-15 19:45:44 +0100

    Update do_share_onedata.py

--- wrappers/do_share_onedata.py
+++ wrappers/do_share_onedata.py
@@ -22,8 +22,83 @@ xmlns_xsi = '"http://www.w3.org/2001/XMLSchema-instance"'
 xsi_schemaLocation = '"http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd"'
 oai_dc_content = 'xmlns:oai_dc=' + xmlns_oaci_dc + ' xmlns:dc=' + xmlns_dc + ' xmlns:xsi=' + xmlns_xsi + ' xsi:schemaLocation=' + xsi_schemaLocation
 
-
 # own functions
+
+def create_dublincore_xml_file(all_level1, local_path, filename):
+    """
+    -------
+    Modules
+    -------
+    Doc, indent
+    -----------
+    Description
+    -----------
+    Creates a "filename.xml" on "local_path" in  DublinCore format 
+    with the elements within "all_level1", the JSON of a Catalog. 
+    ----------
+    Parameters
+    ----------
+    all_level1 : dict that contains the JSON of a Catalog (after json.loads) 
+    local_path : local path where the xlm files are stored
+    filename : Folder name to share and handle.
+    -------
+    Returns
+    -------
+    ....????
+    """
+
+    doc, tag, text = Doc().tagtext()
+
+    with tag('metadata'):
+        with tag('oai_dc:dc', oai_dc_content):
+            with tag('dc:title'):
+                text(all_level1['title'])
+            with tag('dc:creator'):
+                text(all_level1["creator"]['@id'])
+            with tag('dc:date'):
+                text(all_level1["@graph"][1]['prov:endedAtTime'])
+            with tag('dc:subject'):
+                text('High energy astrophysics')
+            with tag('dc:subject'):
+                text('http://astrothesaurus.org/uat/739')
+            with tag('dc:subject'):
+                text('Particle astrophysics')
+            with tag('dc:subject'):
+                text('http://astrothesaurus.org/uat/96')
+            with tag('dc:subject'):
+                text('Astronomical simulations')
+            with tag('dc:subject'):
+                text('http://astrothesaurus.org/uat/1857')
+            with tag('dc:rights'):
+                text('CC BY 4.0')
+            with tag('dc:rights'):
+                text('https://creativecommons.org/licenses/by/4.0/')
+            with tag('dc:rights'):
+                text('LAGO rights')
+            with tag('dc:rights'):
+                text('https://raw.githubusercontent.com/lagoproject/DMP/1.1/rights/lagoCommonRights.jsonld')
+            with tag('dc:description'):
+                text(all_level1["description"])
+            with tag('dc:contributor'):
+                text('EGI Datahub')
+            with tag('dc:instrument'):
+                text('LAGO Observatory')
+            with tag('dc:contact'):
+                text('lago-eosc(at)lagoproject.net')
+            with tag('dc:discipline'):
+                text('Astrophysics and Astronomy')
+            with tag('dc:publisher'):
+                text('LAGO Collaboration')
+
+    result = indent(doc.getvalue(), indentation=' '*4, newline='\n')
+
+    file_object = open(local_path + filename + '.xml', 'w')
+    file_object.write(result)
+    file_object.close()
+    print(filename + " XML file created")
+
+
+
 
 def folder0_content(folder0_id, host, token):
     """
@@ -174,6 +249,8 @@ def folder1_getattrs(handleservice_id, local_path, folder1_id, host, token):
 
 
 
+
+
 def publish_catalog(handleservice_id, local_path, folder1_id, filename, host, token):
     """
     -------
@@ -203,58 +280,11 @@ def publish_catalog(handleservice_id, local_path, folder1_id, filename, host, to
     r_level1 = requests.get(OneData_urljson, headers={'X-Auth-Token': token})
     all_level1 = json.loads(r_level1.text)
 
+    # falta: una funcion que devuelva true o false si pasa o no unos minimos requisitos. 
     if 'dataset' in all_level1:
-
-        doc, tag, text = Doc().tagtext()
-
-        with tag('metadata'):
-            with tag('oai_dc:dc', oai_dc_content):
-                with tag('dc:title'):
-                    text(all_level1['title'])
-                with tag('dc:creator'):
-                    text(all_level1["creator"]['@id'])
-                with tag('dc:date'):
-                    text(all_level1["@graph"][1]['prov:endedAtTime'])
-                with tag('dc:subject'):
-                    text('High energy astrophysics')
-                with tag('dc:subject'):
-                    text('http://astrothesaurus.org/uat/739')
-                with tag('dc:subject'):
-                    text('Particle astrophysics')
-                with tag('dc:subject'):
-                    text('http://astrothesaurus.org/uat/96')
-                with tag('dc:subject'):
-                    text('Astronomical simulations')
-                with tag('dc:subject'):
-                    text('http://astrothesaurus.org/uat/1857')
-                with tag('dc:rights'):
-                    text('CC BY 4.0')
-                with tag('dc:rights'):
-                    text('https://creativecommons.org/licenses/by/4.0/')
-                with tag('dc:rights'):
-                    text('LAGO rights')
-                with tag('dc:rights'):
-                    text('https://raw.githubusercontent.com/lagoproject/DMP/1.1/rights/lagoCommonRights.jsonld')
-                with tag('dc:description'):
-                    text(all_level1["description"])
-                with tag('dc:contributor'):
-                    text('EGI Datahub')
-                with tag('dc:instrument'):
-                    text('LAGO Observatory')
-                with tag('dc:contact'):
-                    text('lago-eosc(at)lagoproject.net')
-                with tag('dc:discipline'):
-                    text('Astrophysics and Astronomy')
-                with tag('dc:publisher'):
-                    text('LAGO Collaboration')
-
-        result = indent(doc.getvalue(), indentation=' '*4, newline='\n')
-
-        file_object = open(local_path + filename + '.xml', 'w')
-        file_object.write(result)
-        file_object.close()
-        print(filename + " XML file created")
-
+        
+        create_dublincore_xml_file(all_level1, local_path, filename)
+        # falta : hay que ver por que necesita el fichero xml esta funcion, tampoco entiendo que quiera llamar a createhandle dentro
         shareinfo_level1 = folder1_getattrs(handleServiceId, local_path, folder1_id, host, token)
 
         if shareinfo_level1:
