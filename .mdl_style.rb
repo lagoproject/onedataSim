579e819976bb34700684a6dac9eefdd3a3abb4dd
AJRubio-Montero <antonio.rubio@ciemat.es>
2022-10-11 15:28:34 +0200

    Update .mdl_style.rb

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -2,7 +2,7 @@ all
 # our changelog does this, by design
 # exclude_rule 'MD003'
 # rule 'MD007', :indent => 3
-rule 'MD013', heading_line_length: "90"
+rule 'MD013', heading_line_length: 400
 # => doesn't work for style
 # rule 'MD029', :style => :ordered
 rule "MD029", style: "ordered"
