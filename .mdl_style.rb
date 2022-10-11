736acc6472b5c5b78c2ad6a6465f264c82266fc0
AJRubio-Montero <antonio.rubio@ciemat.es>
2022-10-11 15:20:27 +0200

    Update .mdl_style.rb

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,9 +1,8 @@
 all
 # our changelog does this, by design
 # exclude_rule 'MD003'
-# default in next version, remove then
-#   "MD003": { "style": "atx_closed" 
 # rule 'MD007', :indent => 3
-# doesn't work for style
+rule 'MD013', :heading_line_length => 90
+# => doesn't work for style
 # rule 'MD029', :style => :ordered
 rule "MD029", style: "ordered"
